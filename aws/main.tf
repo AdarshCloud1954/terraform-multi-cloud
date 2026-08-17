fdc90cf3238a2debbef9fd92b0fcaed78c20fb5f
Adarsh_Boddula <adarsh.boddula.07@gmail.com>
2026-08-17 15:56:05 +0530

    Update main.tf

--- aws/main.tf
+++ aws/main.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr
 
   tags = {
     Name = "terraform-multi-cloud-vpc"
@@ -8,8 +8,8 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "ap-south-1a"
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = var.availability_zone
   map_public_ip_on_launch = true
 
   tags = {
