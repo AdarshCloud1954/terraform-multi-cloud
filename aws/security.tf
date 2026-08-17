d6eb7c235bcc8f28995d6962bfe158600a8a4599
Adarsh_Boddula <adarsh.boddula.07@gmail.com>
2026-08-17 16:55:07 +0530

    Update security.tf
    Remove unrestricted SSH access

--- aws/security.tf
+++ aws/security.tf
@@ -11,15 +11,8 @@ resource "aws_security_group" "web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    description = "Allow SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
+    description = "Allow all outbound traffic"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
