30068e5e2244ad9c34b952e9912129ea9b53798d
Adarsh_Boddula <adarsh.boddula.07@gmail.com>
2026-08-17 16:58:16 +0530

    Update ec2.tf

--- aws/ec2.tf
+++ aws/ec2.tf
@@ -1,9 +1,30 @@
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
 resource "aws_instance" "web" {
-  ami           = "ami-0f58b397bc5c1f2e8"
+  ami           = data.aws_ami.amazon_linux.id
   instance_type = "t3.micro"
 
   subnet_id                   = aws_subnet.public.id
-  vpc_security_group_ids      = [aws_security_group.web.id]
+  vpc_security_group_ids     = [aws_security_group.web.id]
   associate_public_ip_address = true
 
   tags = {
